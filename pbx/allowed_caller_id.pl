fdb92bde0e4dc1b40559c158aecca45a13561c76
Kristoffer Ek <stoffer@skulp.net>
2026-08-03 23:44:36 +0200

    feat(pbx): use My::Number::Phone to normalize caller ID in allowed_caller_id.pl

--- pbx/allowed_caller_id.pl
+++ pbx/allowed_caller_id.pl
@@ -3,20 +3,31 @@
 use strict;
 use Data::Dumper;
 use DBI;
+use My::Number::Phone;
 
 my $caller_id = $ARGV[0];
+
+# Normalize caller_id using My::Number::Phone
+my $phone_obj = My::Number::Phone->new($caller_id);
+
+unless ($phone_obj && $phone_obj->is_valid) {
+	# Invalid phone format
+	exit 1;
+}
+
+my $normalized_caller_id = $phone_obj->compact;
 
 my $dbi = 'DBI:mysql:database=$MYSQL_DATABASE;host=pbx-db;port=3306';
 
 my $dbh = DBI->connect($dbi, '$MYSQL_USER', '$MYSQL_PASSWORD', { mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }) || die $!;
 
-my $quoted_caller_id = $dbh->quote($caller_id);
+my $quoted_caller_id = $dbh->quote($normalized_caller_id);
 
 $dbh->do(qq[INSERT INTO log (`caller_id`, `event`, `unix_time`) VALUES ($quoted_caller_id, 'called', UNIX_TIMESTAMP())]) || die $!;
 
 # check if caller id is present and longer than 8 numbers
-if (length($caller_id) < 8) {
-	die;
+if (length($normalized_caller_id) < 8) {
+	exit 1;
 }
 
 # check if caller id is in db
