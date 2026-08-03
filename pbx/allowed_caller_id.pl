f3ac4369ccd7f2ba3f4b2ed481ca679917b46ae8
Kristoffer Ek <stoffer@skulp.net>
2026-08-04 00:04:41 +0200

    refactor(pbx): match caller ID by suffix pattern instead of My::Number::Phone

--- pbx/allowed_caller_id.pl
+++ pbx/allowed_caller_id.pl
@@ -3,39 +3,34 @@
 use strict;
 use Data::Dumper;
 use DBI;
-use My::Number::Phone;
 
-my $caller_id = $ARGV[0];
+my $caller_id = $ARGV[0] // '';
 
-# Normalize caller_id using My::Number::Phone
-my $phone_obj = My::Number::Phone->new($caller_id);
+# Strip all non-digit characters (e.g. +, spaces, dashes)
+$caller_id =~ s/\D//g;
 
-unless ($phone_obj && $phone_obj->is_valid) {
-	# Invalid phone format
+# Basic sanity check: must contain at least 8 digits
+if (length($caller_id) < 8) {
 	exit 1;
 }
-
-my $normalized_caller_id = $phone_obj->compact;
 
 my $dbi = 'DBI:mysql:database=$MYSQL_DATABASE;host=pbx-db;port=3306';
 
 my $dbh = DBI->connect($dbi, '$MYSQL_USER', '$MYSQL_PASSWORD', { mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }) || die $!;
 
-my $quoted_caller_id = $dbh->quote($normalized_caller_id);
+my $quoted_caller_id = $dbh->quote($caller_id);
+my $like_pattern     = $dbh->quote('%' . $caller_id);
 
+# Log the initial incoming call attempt
 $dbh->do(qq[INSERT INTO log (`caller_id`, `event`, `unix_time`) VALUES ($quoted_caller_id, 'called', UNIX_TIMESTAMP())]) || die $!;
 
-# check if caller id is present and longer than 8 numbers
-if (length($normalized_caller_id) < 8) {
-	exit 1;
-}
-
-# check if caller id is in db
-my $sth = $dbh->prepare(qq[SELECT COUNT(*) FROM `access` WHERE `enabled` \
+# Query access table matching clean database telephone ending with caller_id
+my $sth = $dbh->prepare(qq[SELECT COUNT(*) FROM `access` WHERE `enabled` = 1 \
 	AND ((FROM_UNIXTIME(`start`) <= NOW() AND NOW() <= FROM_UNIXTIME(`end`)) \
 		OR ((`start` IS NULL OR `start` = 0) AND NOW() <= FROM_UNIXTIME(`end`)) \
 		OR ((`start` IS NULL OR `start` = 0) AND (`end` IS NULL OR `end` = 0))) \
-		AND `telephone` = ] . $quoted_caller_id);
+	AND REGEXP_REPLACE(`telephone`, '[^0-9]', '') LIKE $like_pattern]);
+
 $sth->execute || die $!;
 
 if ($sth->fetchrow_array > 0) {
