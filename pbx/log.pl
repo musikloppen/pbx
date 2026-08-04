7ee34f7d1fd0d4acb057cb7feaf6b421c2058135
Kristoffer Ek <stoffer@skulp.net>
2026-08-04 19:50:48 +0200

    refactor: set root build context to share My::Number::Phone and update PBX logging

--- pbx/log.pl
+++ pbx/log.pl
@@ -1,19 +1,39 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
-use Data::Dumper;
+use warnings;
 use DBI;
+use My::Number::Phone;
 
-my $caller_id = $ARGV[0];
-my $event = $ARGV[1];
+my $raw_caller_id = $ARGV[0];
+my $event         = $ARGV[1];
 
-my $dbi = 'DBI:mysql:database=$MYSQL_DATABASE;host=pbx-db;port=3306';
+# Normalize phone number if present
+my $normalized_caller_id = $raw_caller_id;
+if ($raw_caller_id) {
+	my $phone_obj = My::Number::Phone->new($raw_caller_id);
+	if ($phone_obj && $phone_obj->is_valid) {
+		$normalized_caller_id = $phone_obj->compact; # or ->international
+	}
+}
 
-my $dbh = DBI->connect($dbi, '$MYSQL_USER', '$MYSQL_PASSWORD', { mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }) || die $!;
+my $db_host = $ENV{DB_HOST} || 'pbx-db';
+my $db_name = $ENV{MYSQL_DATABASE} || 'pbx';
+my $db_user = $ENV{MYSQL_USER} || 'pbx';
+my $db_pass = $ENV{MYSQL_PASSWORD} || '';
 
-my $quoted_caller_id = $dbh->quote($caller_id);
-my $quoted_event = $dbh->quote($event);
+my $dbh = DBI->connect(
+	"DBI:mysql:database=$db_name;host=$db_host;port=3306",
+	$db_user,
+	$db_pass,
+	{ RaiseError => 0, PrintError => 1, mysql_auto_reconnect => 1, mysql_enable_utf8 => 1 }
+) or die $!;
 
-$dbh->do(qq[INSERT INTO log (`caller_id`, `event`, `unix_time`) VALUES ($quoted_caller_id, $quoted_event, UNIX_TIMESTAMP())]) || warn $!;
+my $sth = $dbh->prepare(qq[
+	INSERT INTO log (`caller_id`, `event`, `unix_time`) 
+	VALUES (?, ?, UNIX_TIMESTAMP())
+]);
+
+$sth->execute($normalized_caller_id, $event) or warn $!;
 
 1;
