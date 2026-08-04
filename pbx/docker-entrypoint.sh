dcb4f2532d048173953ccbf8e72171ef99478bc7
Kristoffer Ek <stoffer@skulp.net>
2026-08-04 16:09:59 +0200

    feat(pbx): implement DEBUG dry-run dialplan interception and SIP trunk isolation

--- pbx/docker-entrypoint.sh
+++ pbx/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+export DEBUG="${DEBUG:-0}"
+
 # List of required environment variables
 REQUIRED_VARS="
 SIP_TRUNK_USERNAME
@@ -33,10 +35,21 @@ fi
 
 echo "[ENTRYPOINT] All required environment variables are set. Applying templates..."
 
+# --- DEBUG TRUNK ISOLATION OVERRIDE ---
+if [ "$DEBUG" = "1" ] || [ "$DEBUG" = "true" ]; then
+	echo "[ENTRYPOINT DEBUG] DEBUG MODE IS ACTIVE: Neutralizing SIP registration & host to prevent kicking production!"
+	EFFECTIVE_SIP_HOST="127.0.0.1"
+	EFFECTIVE_SIP_REGISTER="; registration disabled in DEBUG mode"
+else
+	EFFECTIVE_SIP_HOST="$SIP_TRUNK_HOST"
+	EFFECTIVE_SIP_REGISTER="register => $SIP_TRUNK_USERNAME:$SIP_TRUNK_SECRET@$SIP_TRUNK_HOST/$SIP_TRUNK_USERNAME"
+fi
+
 # 2. Replace environment variables in Asterisk SIP config
-perl -pi -e 's/\$SIP_TRUNK_USERNAME/$ENV{SIP_TRUNK_USERNAME}/g' /etc/asterisk/sip.conf
-perl -pi -e 's/\$SIP_TRUNK_SECRET/$ENV{SIP_TRUNK_SECRET}/g' /etc/asterisk/sip.conf
-perl -pi -e 's/\$SIP_TRUNK_HOST/$ENV{SIP_TRUNK_HOST}/g' /etc/asterisk/sip.conf
+perl -pi -e 's/\$SIP_TRUNK_USERNAME/$SIP_TRUNK_USERNAME/g' /etc/asterisk/sip.conf
+perl -pi -e 's/\$SIP_TRUNK_SECRET/$SIP_TRUNK_SECRET/g' /etc/asterisk/sip.conf
+perl -pi -e 's/\$SIP_TRUNK_HOST/$EFFECTIVE_SIP_HOST/g' /etc/asterisk/sip.conf
+perl -pi -e 's/\$SIP_REGISTER_LINE/$EFFECTIVE_SIP_REGISTER/g' /etc/asterisk/sip.conf
 
 # 3. Replace environment variables in Asterisk Manager (AMI) config
 perl -pi -e 's/\$ASTERISK_AMI_USER/$ENV{ASTERISK_AMI_USER}/g' /etc/asterisk/manager.conf
@@ -45,6 +58,7 @@ perl -pi -e 's/\$ASTERISK_AMI_PASS/$ENV{ASTERISK_AMI_PASS}/g' /etc/asterisk/mana
 # 4. Replace environment variables in Asterisk Extensions (Dialplan) config
 perl -pi -e 's/\$GATE_1_PHONE/$ENV{GATE_1_PHONE}/g' /etc/asterisk/extensions.conf
 perl -pi -e 's/\$GATE_2_PHONE/$ENV{GATE_2_PHONE}/g' /etc/asterisk/extensions.conf
+perl -pi -e 's/\$DEBUG/$ENV{DEBUG}/g' /etc/asterisk/extensions.conf
 
 # 5. Replace environment variables in Perl scripts
 perl -pi -e 's/\$MYSQL_DATABASE/$ENV{MYSQL_DATABASE}/g' /var/lib/asterisk/allowed_caller_id.pl
